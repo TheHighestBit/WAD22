789952f55b8d2e71be512e8c6ebca56f30a371da
v2int5 <mihkelv5@gmail.com>
2022-11-22 17:42:46 +0200

    fixed naming issue login -> signup

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar">
     <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/signup">Register</router-link>
   </nav>
 </template>
 
